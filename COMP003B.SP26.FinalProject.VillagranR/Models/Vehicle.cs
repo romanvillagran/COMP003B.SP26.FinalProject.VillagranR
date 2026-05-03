b1c7fa68fea1d9fb477f9bdf5869c3b4f32a1f84
cbig3 <romanvillagran386@gmail.com>
2026-05-02 23:18:40 -0700

    added code to the vehicle model that askd for the make, model, year, license plate, and the mileage

--- COMP003B.SP26.FinalProject.VillagranR/Models/Vehicle.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Models/Vehicle.cs
@@ -1,6 +1,35 @@
-﻿namespace COMP003B.SP26.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Models
 {
     public class Vehicle
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string? Make { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string? Model { get; set; }
+
+        [Range(1950,2027)]
+        public int Year { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string?  LicensePlate { get; set; }
+
+        [Required]
+        [Range(0,500000)]
+        public int Mileage { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public virtual Customer? Customer { get; set; }
+
+        public ICollection<ServiceAppointment>? ServiceAppointment { get; set; }
+
     }
 }
