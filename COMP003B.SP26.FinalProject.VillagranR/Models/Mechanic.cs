2ead8a5d7458ee8f33c3cc5fe980fc1b4e734614
cbig3 <romanvillagran386@gmail.com>
2026-05-02 23:39:20 -0700

    added code to the mechanic  model file where it required names, specialty, how much experience, certification numnber, whether there available, and the shop they work at

--- COMP003B.SP26.FinalProject.VillagranR/Models/Mechanic.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Models/Mechanic.cs
@@ -1,6 +1,30 @@
-﻿namespace COMP003B.SP26.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Models
 {
     public class Mechanic
     {
+        public int MechanicId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Specialty { get; set; }
+
+        [Range(0,100)]
+        public int YearsExperience { get; set; }
+
+        [Required]
+        public string? CertificationNumber { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        [StringLength(100)]
+        public string? ShopLocation { get; set; }
+
+        public ICollection<ServiceAppointment>? serviceAppointments { get; set; }
     }
 }
