7b17fcd24f0c6f2254cc4db7a2fffaa661514a87
cbig3 <romanvillagran386@gmail.com>
2026-05-02 23:54:30 -0700

    added code to the service appointment mmodel file

--- COMP003B.SP26.FinalProject.VillagranR/Models/ServiceAppointment.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Models/ServiceAppointment.cs
@@ -2,5 +2,12 @@
 {
     public class ServiceAppointment
     {
+        public int ServiceAppointmentId {  get; set; }
+
+        public virtual Vehicle? Vehicle { get; set; }
+
+        public virtual Mechanic? Mechanic { get; set; }
+
+        public virtual ServiceType? ServiceType { get; set; }
     }
 }
