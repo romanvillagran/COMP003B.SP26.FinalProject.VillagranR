1baf2685339ebe65fe4bf7ddf513f9838429e33e
cbig3 <romanvillagran386@gmail.com>
2026-05-03 00:03:25 -0700

    first i created both the data folder and the ApplicationDbContext file, then i added all the DbSet code for customer, Vehicle, mechanic, servicetype, and the appointment

---
+++ COMP003B.SP26.FinalProject.VillagranR/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using COMP003B.SP26.FinalProject.VillagranR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Vehicle> vehicles { get; set; }
+        public DbSet<Mechanic > mechanics { get; set; }
+
+        public DbSet<ServiceType> serviceTypes { get; set; }
+
+        public DbSet<ServiceAppointment> serviceAppointment { get; set; }
+    }
+}
