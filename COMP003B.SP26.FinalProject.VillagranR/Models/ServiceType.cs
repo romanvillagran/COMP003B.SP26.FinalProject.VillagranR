4692bfb75fd641dcef9a440d796dbcd85f91c036
cbig3 <romanvillagran386@gmail.com>
2026-05-02 23:46:24 -0700

    added code to the service type file where it ask for what kind of service the car needs, a quick description, how much it might cost, how long it will take, and whether it is still active

--- COMP003B.SP26.FinalProject.VillagranR/Models/ServiceType.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Models/ServiceType.cs
@@ -1,6 +1,26 @@
-﻿namespace COMP003B.SP26.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Models
 {
     public class ServiceType
     {
+        public int ServiceTypeId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? ServiceName { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Range(0, 5000)]
+        public int Price { get; set; }
+
+        public int EstimatedHours { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public ICollection<ServiceAppointment>? ServiceAppointments { get; set; }
     }
 }
