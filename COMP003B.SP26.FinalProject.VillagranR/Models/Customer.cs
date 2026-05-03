4708d1c425f43ab7df2087def1e3042f50fad280
cbig3 <romanvillagran386@gmail.com>
2026-05-02 22:32:52 -0700

    added the code to customer which requires you to add your name, email, and phone. I am taking a page from the assignment where i have to add the ? after each public whatever and the ICollection otherwise it will not work like in the assignment

--- COMP003B.SP26.FinalProject.VillagranR/Models/Customer.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Models/Customer.cs
@@ -1,6 +1,23 @@
-﻿namespace COMP003B.SP26.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Address { get; set; }
+
+        [Required]
+        [Phone]
+        public string? Phone { get; set; }
+        
+        public ICollection<Vehicle>? Vehicles { get; set; }
     }
 }
