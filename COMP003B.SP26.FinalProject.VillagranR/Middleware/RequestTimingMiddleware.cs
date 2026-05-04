2494bed9562e24a2b84649baecccb574ee93f7ea
cbig3 <romanvillagran386@gmail.com>
2026-05-03 22:52:12 -0700

    added code to thye middleware file

--- COMP003B.SP26.FinalProject.VillagranR/Middleware/RequestTimingMiddleware.cs
+++ COMP003B.SP26.FinalProject.VillagranR/Middleware/RequestTimingMiddleware.cs
@@ -1,6 +1,15 @@
 ﻿namespace COMP003B.SP26.FinalProject.VillagranR.Middleware
 {
-    public class RequestTimingMiddleware
+    public class RequestTimingMiddleware(RequestDelegate next)
     {
+        private readonly RequestDelegate _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Request] {context.Response.StatusCode}");
+        }
+
     }
 }
