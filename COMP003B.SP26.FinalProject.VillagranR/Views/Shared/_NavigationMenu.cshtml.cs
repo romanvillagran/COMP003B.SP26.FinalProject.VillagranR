44e27022162a3cf504c6f4818d7c0f69d36151bf
cbig3 <romanvillagran386@gmail.com>
2026-05-05 14:07:26 -0700

    forgot that i needed partial views so i made the file and moved everything from the layout to the _Navigationmenu.cshtml file

---
+++ COMP003B.SP26.FinalProject.VillagranR/Views/Shared/_NavigationMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.SP26.FinalProject.VillagranR.Views.Shared
+{
+    public class _NavigationMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
